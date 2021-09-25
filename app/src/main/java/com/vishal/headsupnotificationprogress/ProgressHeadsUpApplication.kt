ef7ff5f45f3f1fc60de3be6ac4ad032f16fab44e
Vishal <vambre21@gmail.com>
2021-09-25 14:07:34 +0530

    Fix koin issue and bump version code and version name

--- app/src/main/java/com/vishal/headsupnotificationprogress/ProgressHeadsUpApplication.kt
+++ app/src/main/java/com/vishal/headsupnotificationprogress/ProgressHeadsUpApplication.kt
@@ -5,12 +5,13 @@ import com.vishal.headsupnotificationprogress.di.prefModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class ProgressHeadsUpApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR) //Investigate: https://github.com/InsertKoinIO/koin/issues/847
             androidContext(this@ProgressHeadsUpApplication)
             modules(
                 prefModule
